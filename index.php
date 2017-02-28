4c3b17ffec04e151a2899737a77dac8aaef05355
vera23 <veramir10@gmail.com>
2017-03-01 00:00:53 +0300

    init

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="slider">
+<img src="assets/images/background.png">
+</div>
+    <div class="row" id="header">
+        <div class="container" id="no-opacity">
+        <div class="col-lg-4 col-xs-12 col-sm-4">
+            <img src="assets/images/logo.png">
+        </div>
+        <div class="col-lg-5 col-sm-5 col-xs-12">
+            <ul class="nav navbar-nav">
+                <li><a href="#">
+                        Главная
+                    </a>
+                </li>
+                <li><a href="#">
+                        Каталог </a>
+                </li>
+                <li><a href="#">
+                        О компании </a>
+                </li>
+                <li><a href="#">
+                        Новости</a>
+                </li>
+                <li><a href="#">
+                        Контакты </a>
+                </li>
+
+            </ul>
+        </div>
+        <div class="hidden-xs col-lg-3 col-sm-3">
+            <div class="text-right col-sm-12">
+                <span class="fsize-9">тел.:</span>
+                <span class="fcolor-red fsize-13">(495)</span>
+                <span class="fcolor-blue fsize-18">955-01-91</span>
+            </div>
+            <div class="text-right col-sm-12">
+                <span class="fsize-9">email:</span>
+                <span class="fcolor-blue fsize-9">veramir10@gmail.com</span>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="row" id="news">
+    <div class="col-xs-12 col-lg-12 col-md-4"></div>
+</div>
+<div id="providers"></div>
+<div id="footer"></div>
+</body>
+</html>
